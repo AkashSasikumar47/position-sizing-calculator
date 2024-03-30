6dfd610f5d71b23ec80c6c42f4306fc8de32034b
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-03-30 14:43:32 +0530

    Update components

---
+++ src/components/Appcopy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "./HomePage/Header";
+import QuickLook from "./HomePage/QuickLook";
+import Feed from "./HomePage/Feed";
+import Footer from "./HomePage/Footer";
+
+const Appcopy = () => {
+	return (
+		<div>
+			<Header />
+			<QuickLook />
+			<Feed />
+			<Footer />
+		</div>
+	)
+}
+
+export default Appcopy
---
+++ src/components/Clubs/ClubsHero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ClubsHero = () => {
+    return (
+        <div>ClubsHero</div>
+    )
+}
+
+export default ClubsHero
